0a37f93a66285528c579970969a65f25ef22d63e
vallettafox <43784504+vallettafox@users.noreply.github.com>
2019-01-09 15:55:23 -0500

    First Commit

---
+++ L9P2.py
@@ -0,0 +1,9 @@
+# the correct password is 'Open Sesame'
+print('Locker Bot:')
+userpassword = input('What is your password? ')
+if userpassword == 'Open Sesame':
+	print('Password is valid!')
+	print('Locker Opening')
+else:
+	print('Password invalid ... enabling laser defense system, please stand down.')
+	print('Pew Pew Pew!')
